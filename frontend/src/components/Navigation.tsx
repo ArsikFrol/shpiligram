d5d2c75af92407a4c7dfcc326eb5e87d33bb21d7
ArseniyFrolov <arsik.frol.555@gmail.com>
2026-06-06 10:39:52 +0500

    add_navigation

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,8 +1,12 @@
+'use client'
+
 import { JSX } from 'react/jsx-runtime'
 import { TRout } from '@/types/router'
 import { CircleUser, MessageSquare, Settings, UserRoundPen } from 'lucide-react'
 import Link from 'next/link'
 import { cn } from '@/lib/utils'
+import { cloneElement, useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 type TElem = {
     id: number,
@@ -12,25 +16,44 @@ type TElem = {
 }
 
 const listNav: TElem[] = [
-    { id: 1, text: 'Chats', elem: <MessageSquare strokeWidth={1.25} color='white' size={25} />, link: '/' },
-    { id: 2, text: 'Contacts', elem: < CircleUser strokeWidth={1.25} color='white' size={25} />, link: '/contacts' },
-    { id: 3, text: 'Settings', elem: <Settings strokeWidth={1.25} color='white' size={25} />, link: '/settings' },
-    { id: 4, text: 'Profile', elem: <UserRoundPen strokeWidth={1.25} color='white' size={25} />, link: '/profile' }
+    { id: 1, text: 'Chats', elem: <MessageSquare strokeWidth={2} color='white' size={25} />, link: '/' },
+    { id: 2, text: 'Contacts', elem: < CircleUser strokeWidth={2} color='white' size={25} />, link: '/contacts' },
+    { id: 3, text: 'Settings', elem: <Settings strokeWidth={2} color='white' size={25} />, link: '/settings' },
+    { id: 4, text: 'Profile', elem: <UserRoundPen strokeWidth={2} color='white' size={25} />, link: '/profile' }
 ]
 
 export default function Navigation() {
+    const router = useRouter()
+
+    const [activeElem, setActiveElem] = useState<number>(1)
+
+    const clickNav = (obj: TElem) => {
+        setActiveElem(obj.id)
+        router.push(obj.link)
+    }
+
     return (
-        <div className='bg-[#202D3D] rounded-2xl flex items-center gap-x-[50px] px-[50px] py-[20px] shadow'>
+        <div className='bg-[#202D3D] rounded-4xl flex items-center gap-x-[10px] px-[10px] py-[10px] shadow'>
             {
                 listNav.map((obj, index: number) => {
+                    const isActive = activeElem === obj.id
+                    const activeColor = 'oklch(62.3% 0.214 259.815)'
+
                     return (
-                        <Link href={obj.link} className={cn(
-                            "flex flex-col items-center gap-y-[10px]",
-                            'hover:scale-105 transition-transform duration-300 cursor-pointer'
-                        )} key={index}>
-                            {obj.elem}
-                            <div className='text-white font-medium text-[15px]'>{obj.text}</div>
-                        </Link>
+                        <div className={cn(
+                            "flex flex-col items-center gap-y-[5px] w-[110px] py-[10px]",
+                            'hover:scale-105 transition-transform duration-300 cursor-pointer',
+                            isActive && 'bg-active-bg rounded-4xl'
+                        )} key={index} onClick={() => clickNav(obj)}>
+                            {isActive
+                                ? cloneElement(obj.elem, { color: activeColor })
+                                : obj.elem
+                            }
+                            <div className={cn(
+                                'text-white font-semibold text-[15px]',
+                                isActive && 'text-blue-500'
+                            )}>{obj.text}</div>
+                        </div>
                     )
                 })
             }
